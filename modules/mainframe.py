6567751d259fd921f6c81d1f9c2c72af6427dc0a
Name <nick20122044@gmail.com>
2024-10-05 13:32:06 +0300

    Set size our app

--- modules/mainframe.py
+++ modules/mainframe.py
@@ -1,4 +1,13 @@
 import customtkinter
+from .read_json import read_json
+import json
 
 main_frame = customtkinter.CTk(fg_color= ('#FE2EF7'))
 
+config = read_json(name_json= 'config.json')
+# print(json.dumps(config, indent= 4))
+#
+WIDTH = config['mainframe']['width']
+HEIGHT = config['mainframe']['height']
+#
+main_frame.geometry(f'{WIDTH}x{HEIGHT}')
